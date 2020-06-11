71d634061222768872ea0919b26c83b488220036
vijaybhaskarreddyn <vijayreddy4277@gmail.com>
2020-06-11 09:20:45 +0530

    Add FindByMethods

--- 06-spring-data-jpa/src/main/java/com/vijay/Application.java
+++ 06-spring-data-jpa/src/main/java/com/vijay/Application.java
@@ -1,5 +1,9 @@
 package com.vijay;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,11 +17,42 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctxt = SpringApplication.run(Application.class, args);
 		ContactsMasterRepo bean = ctxt.getBean(ContactsMasterRepo.class);
-		System.out.println("++++++======>  " + bean.getClass().getName());
-		ContactsMasterEntity entity = new ContactsMasterEntity(103, "VijayaBhaskar", 123459048);
-		bean.save(entity);
-		//System.out.println("Total No.Of Records in table " + bean.count());
-		System.out.println("Entity Saved Sucessfully " + entity.toString());
+
+		/*
+		 * List<ContactsMasterEntity> entities =
+		 * bean.findByContactNameAndContactNumber("VijayaBhaskarReddy", 123459048);
+		 * entities.forEach(entity -> { System.out.println(entity); });
+		 */
+
+		/*
+		 * Optional<ContactsMasterEntity> entity = bean.findById(101);
+		 * System.out.println(entity);
+		 */
+
+		Iterable<ContactsMasterEntity> entities = bean.findAllById(Arrays.asList(101, 102));
+		entities.forEach(entity -> {
+			System.out.println(entity);
+		});
+		/*
+		 * ContactsMasterEntity entity = bean.findByContactName("VijayaBhaskarReddy");
+		 * System.out.println(entity);
+		 */
+		
+		
+		/*
+		 * Iterable<ContactsMasterEntity> entities = bean.findAll();
+		 * entities.forEach(entity -> { System.out.println(entity); });
+		 */
+
+		
+		
+		/*
+		 * System.out.println("++++++======>  " + bean.getClass().getName());
+		 * ContactsMasterEntity entity = new ContactsMasterEntity(103, "VijayaBhaskar",
+		 * 123459048); bean.save(entity);
+		 * System.out.println("Total No.Of Records in table " + bean.count());
+		 * System.out.println("Entity Saved Sucessfully " + entity.toString());
+		 */
 		ctxt.close();
 	}
 
--- 06-spring-data-jpa/src/main/java/com/vijay/repo/ContactsMasterRepo.java
+++ 06-spring-data-jpa/src/main/java/com/vijay/repo/ContactsMasterRepo.java
@@ -1,6 +1,7 @@
 package com.vijay.repo;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,8 @@ import com.vijay.entities.ContactsMasterEntity;
 @Repository
 public interface ContactsMasterRepo extends CrudRepository<ContactsMasterEntity, Serializable> {
 
-	
+	public ContactsMasterEntity findByContactName(String name);
+
+	public List<ContactsMasterEntity> findByContactNameAndContactNumber(String name, long num);
+
 }
